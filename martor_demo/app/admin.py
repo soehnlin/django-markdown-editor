fc99c7ab89e3520b2fd405e776095e4d2c26ddd2
Jeremy Soehnlin <jsoeh001@odu.edu>
2020-09-10 21:28:30 -0400

    fixed style issue on admin.py

--- martor_demo/app/admin.py
+++ martor_demo/app/admin.py
@@ -8,6 +8,8 @@ from app.models import Post
 
 
 class PostAdmin(admin.ModelAdmin):
+
+
     list_display = ['title', 'id']
     formfield_overrides = {
         MartorField: {'widget': AdminMartorWidget},
