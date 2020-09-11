9c1cdaeb4345381d4466adcc4c2c74554e59ae4e
soehnlin <soehnlin@gmail.com>
2020-09-10 21:32:10 -0400

    Update admin.py

--- martor_demo/app/admin.py
+++ martor_demo/app/admin.py
@@ -7,11 +7,9 @@ from martor.models import MartorField
 from app.models import Post
 
 
-
 class PostAdmin(admin.ModelAdmin):
-
     
-
+    
     list_display = ['title', 'id']
     formfield_overrides = {
         MartorField: {'widget': AdminMartorWidget},
